e0ae7d90a40d6ff4a145daa6b9dcc15cd6036787
Alexandr <Shyrik.ru@tut.by>
2026-02-06 17:32:09 +0300

    add material, ag chart

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { AllCommunityModule as ChartCommunityModule, ModuleRegistry as ChartModuleRegistry } from 'ag-charts-community';
+import { AllCommunityModule as GridCommonModule, ModuleRegistry as GridModuleRegistry } from 'ag-grid-community';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+// Register all Community features
+GridModuleRegistry.registerModules([GridCommonModule]);
+ChartModuleRegistry.registerModules([ChartCommunityModule]);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
